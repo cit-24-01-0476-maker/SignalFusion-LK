21247610bcab108343310be7539544c28831c4c3
SignalFusion LK <team@signalfusion.lk>
2026-09-20 12:54:26 +0530

    fix(deploy): remove vercel.json and provide index.js and server.js universal entrypoints

---
+++ server.js
@@ -0,0 +1,27 @@
+﻿const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const prepared = app.prepare();
+
+module.exports = async (req, res) => {
+  await prepared;
+  const parsedUrl = parse(req.url, true);
+  await handle(req, res, parsedUrl);
+};
+
+if (require.main === module) {
+  prepared.then(() => {
+    const port = process.env.PORT || 3000;
+    createServer((req, res) => {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    }).listen(port, () => {
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  });
+}
